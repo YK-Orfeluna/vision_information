73d66d2c2b1658961b69753a55616f70f1bc2bb6
YK-Orfeluna <k030913@kansai-u.ac.jp>
2018-01-29 09:32:59 +0900

    optical illusion

--- visual_information_2/graph.py
+++ visual_information_2/graph.py
@@ -14,7 +14,10 @@ xlabel = "distance of radius between base-circle and stimulation-circle (pixel)"
 
 # accuracy
 y = np.array([0.00, 0.00, 0.25, 0.80, 1.00, 1.00, 1.00], dtype=np.float64)
-sem = np.array([0, 0, 0.099339927, 0.091766294, 0, 0, 0], dtype=np.float64)
+#sem = np.array([0, 0, 0.099339927, 0.091766294, 0, 0, 0], dtype=np.float64)
+sem_y = np.array([0.25, 0.80], dtype=np.float64)
+sem_x = np.array([20, 30], dtype=np.uint64)
+sem = np.array([.099339927, 0.091766294], dtype=np.float64)
 ylabel = "mean accuracy"
 
 title = "result of experiment: optical illusion"
@@ -48,7 +51,7 @@ for i in search_x :
 plt.figure()
 
 plt.plot(x, y, marker="o", color="blue")
-plt.errorbar(x, y, fmt="b", yerr=sem, ecolor='blue', capsize=4, label="accuracy: mean&SEM")
+plt.errorbar(sem_x, sem_y, fmt="bo", yerr=sem, ecolor='blue', capsize=4, label="accuracy: mean&SEM")
 
 # drawing auxiliary line: y=0.5
 plt.hlines([0.5], xmin, xmax, color="gray", linestyles='dashed', label="auxiliary line: "+r"($y=0.5$)")
@@ -70,10 +73,11 @@ plt.text(45, 0.8, "x0=%s, \nk=%s" %(popt[0], popt[1]), fontsize=8)
 
 plt.text(13, 0.55, "(%.1f, 0.5)" %target)
 
-if 1 :
+if 0 :
 	plt.show()
 else :
-	plt.savefig("rslt.png", dpi=300)
-	print("saved figure: rslt.png")
+	figname = "rslt.png"
+	plt.savefig(figname, dpi=300)
+	print("saved figure: %s" %figname)
 
 exit()
